03423c8f53dd0ec2d0a312e8a89af5c3f1fae877
afaris01 <sidukertas0@gmail.com>
2022-06-29 17:52:55 +0700

    update heroku

--- database/db.go
+++ database/db.go
@@ -38,6 +38,19 @@ func MulaiDB() {
 	DB = database
 }
 
+// func MulaiDB() {
+
+// 	dsn := "root@tcp(127.0.0.1:3306)/mygram?charset=utf8mb4&parseTime=True&loc=Local"
+// 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatal("Gagal menyambung ke database :", err)
+// 	}
+
+// 	fmt.Println("Koneksi Sukses")
+// 	database.Debug().AutoMigrate(models.User{}, models.Photo{}, models.Comment{}, models.SocialMedia{})
+// 	DB = database
+// }
+
 func AmbilDB() *gorm.DB {
 	return DB
 }
