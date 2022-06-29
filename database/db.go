9b016192317f154e1ae86e6554e663687353bf51
afaris01 <sidukertas0@gmail.com>
2022-06-29 20:18:00 +0700

    update

--- database/db.go
+++ database/db.go
@@ -4,24 +4,23 @@ import (
 	"final-project-2/models"
 	"fmt"
 	"log"
+	"os"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var (
-	host     = "localhost"
-	user     = "root"
-	password = ""
-	dbPort   = "443"
-	dbName   = "mygram"
-	DB       *gorm.DB
-	err      error
-)
+var DB *gorm.DB
 
 func MulaiDB() {
-	dsn := "root@tcp(127.0.0.1:3306)/mygram?charset=utf8mb4&parseTime=True&loc=Local"
-	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, dbPort, dbName)
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Gagal menyambung ke database :", err)
 	}
